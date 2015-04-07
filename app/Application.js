884e145780d518b2969a0b568dfd9bf71af27855
gregsilverman <greg.marc.silverman@gmail.com>
2015-04-07 14:50:39 -0500

    added splash screen

--- app/Application.js
+++ app/Application.js
@@ -29,6 +29,16 @@ Ext.define('cardioCatalogQT.Application', {
         'cardioCatalogQT.service.Base64'
     ],
 
+    //autoCreateViewport: true,
+
+    // create a reference in Ext.application so we can access it from multiple functions
+    splashscreen: {},
+
+    init: function() {
+        // start the mask on the body and get a reference to the mask
+        splashscreen = Ext.getBody().mask('Loading cardioCatalogQT, please stand by ...', 'splashscreen');
+    },
+
     launch: function () {
 
         Ext.Loader.setConfig({enabled: true});
@@ -48,6 +58,25 @@ Ext.define('cardioCatalogQT.Application', {
             apiGetQ: '/api/getQ/'
 
         };
+
+
+        Ext.tip.QuickTipManager.init();
+        var task = new Ext.util.DelayedTask(function() {
+
+            // fade out the body mask
+            splashscreen.fadeOut({
+                duration: 500,
+                remove: true
+            });
+
+            // fade out the message
+            splashscreen.next().fadeOut({
+                duration: 500,
+                remove: true
+            });
+        });
+
+        task.delay(1000);
 
         // TODO - Launch the application
 
