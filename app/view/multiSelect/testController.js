9332ac9e8390a67ba4aeebe68dd30e6f847b5b31
gms <gms@umn.edu>
2015-04-16 14:20:21 -0500

    house cleaning

--- app/view/multiSelect/testController.js
+++
@@ -1,5 +0,0 @@
-Ext.define('cardioCatalogQT.view.multiSelect.testController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.multiselect-test'
-    
-});
--- app/view/multiSelect/testModel.js
+++
@@ -1,8 +0,0 @@
-Ext.define('cardioCatalogQT.view.multiSelect.testModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.multiselect-test',
-    data: {
-        name: 'cardioCatalogQT'
-    }
-
-});
