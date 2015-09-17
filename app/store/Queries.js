e2cadf642ec5f0e1d109daa57c5a8cc3de210d22
gregsilverman <greg.marc.silverman@gmail.com>
2015-09-16 19:41:02 -0500

    fix queries store url

--- app/store/Queries.js
+++ app/store/Queries.js
@@ -9,7 +9,7 @@ Ext.define('cardioCatalogQT.store.Queries', {
 
             proxy: {
             type: 'rest',
-                url: 'http://127.0.0.1:5000/remote_query_get',
+                url: 'http://cc.cardio.umn.edu/api/remote_query_get',
                 reader: {
                 type: 'json',
                     rootProperty: 'cc_query'
